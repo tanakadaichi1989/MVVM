c762cd43ee48735c940385b61a001cdb644e9156
Daichi Tanaka <tanakadaichi_1989@icloud.com>
2022-05-14 00:56:22 +0900

    Initial Commit

---
+++ MVVM/MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMApp.swift
+//  MVVM
+//
+//  Created by 田中大地 on 2022/05/14.
+//
+
+import SwiftUI
+
+@main
+struct MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
